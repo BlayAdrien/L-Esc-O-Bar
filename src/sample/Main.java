db6472b5740ee8c7200c45232e8eebbbd4a02bc9
AdrienBlay <Adrien.BLAY@etu.uca.fr>
2020-12-04 16:32:34 +0100

    Rajouté page d'accueil et 3 boutons

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -9,23 +9,25 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
     public static Partie p;
+    private static Stage primaryStage;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         try{
             p = new Partie(300);
             p.genererCommandes();
-            Parent root = FXMLLoader.load(getClass().getResource("../Vue/jeu.fxml"));
-            primaryStage.setTitle("L'Esc Ô Bar");
+            Parent root = FXMLLoader.load(getClass().getResource("../Vue/Accueil.fxml"));
+            primaryStage.setTitle("L'Esc O Bar");
             primaryStage.setScene(new Scene(root));
             primaryStage.show();
         }catch(Exception e) {
             e.printStackTrace();
         }
-
-
     }
 
+    public static Stage getPrimaryStage() {
+        return primaryStage;
+    }
 
     public static void main(String[] args) {
         launch(args);
