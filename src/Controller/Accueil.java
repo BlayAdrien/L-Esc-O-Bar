2a93523562ecf000be33194bd3bbb82a0811f793
AdrienBlay <Adrien.BLAY@etu.uca.fr>
2020-12-09 23:44:25 +0100

    La traduction marche enfin

--- src/Controller/Accueil.java
+++ src/Controller/Accueil.java
@@ -1,18 +1,52 @@
 package Controller;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 import sample.Main;
 
 
 public class Accueil {
 
+	@FXML
+	private Label rules;
+	@FXML
+	private Label play;
+	@FXML
+	private Label quit;
+
+
+	private ResourceBundle bundle;
+	private Locale locale;
 	public Button closeButton;
+
+	@FXML
+	private void btnEn(ActionEvent event){
+		loadLang("en");
+	}
+
+	@FXML
+	private void btnFr(ActionEvent event){
+		loadLang("fr");
+	}
+
+	@FXML
+	private void loadLang(String lang){
+		locale = new Locale(lang);
+		bundle = ResourceBundle.getBundle("Vue.labelText", locale);
+		play.setText(bundle.getString("play"));
+		rules.setText(bundle.getString("rules"));
+		quit.setText(bundle.getString("quit"));
+	}
 
 	@FXML
     private void quittez() {
--- src/Controller/RegleDuJeu.java
+++ src/Controller/RegleDuJeu.java
@@ -1,5 +1,51 @@
 package Controller;
 
+import java.io.IOException;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+import sample.Main;
+
 public class RegleDuJeu {
 
+
+	@FXML
+	private Label rules;
+	@FXML
+	private Label description;
+
+	private ResourceBundle bundle;
+	private Locale locale;
+	public Button closeButton;
+
+	@FXML
+	private void btnEn(ActionEvent event){
+		loadLang("en");
+	}
+
+	@FXML
+	private void btnFr(ActionEvent event){
+		loadLang("fr");
+	}
+
+	@FXML
+	private void loadLang(String lang){
+		locale = new Locale(lang);
+		bundle = ResourceBundle.getBundle("Vue.labelText", locale);
+		rules.setText(bundle.getString("rules"));
+		description.setText(bundle.getString("description"));
+	}
+
+	@FXML
+    private void retourAccueil() {
+		Stage stage = (Stage) closeButton.getScene().getWindow();
+        stage.close();
+    }
 }
